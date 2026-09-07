2c6709c83fb2013b28613efee967946414d9989d
Lari Nurkka <lari.nurkka@gmail.com>
2026-09-07 14:13:07 +0000

    image directory listing

--- src/api.rs
+++ src/api.rs
@@ -1,15 +1,63 @@
+use std::{fs::FileType, path::Path};
+use futures_lite::stream::StreamExt;
+
 use actix_web::{Error, HttpResponse, get, web};
+use serde::Serialize;
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/api/v1")
+            .service(hello_world)
+            .service(get_directory),
+    );
+}
 
 #[get("/hello")]
 pub async fn hello_world() -> Result<HttpResponse, Error> {
     Ok(HttpResponse::Ok().body("Hello world!"))
 }
 
-pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::scope("/api/v1")
-            .service(hello_world),
-    );
+#[derive(Debug, Serialize)]
+struct DirectoryListing {
+    info: Option<String>,
+    files: Vec<String>,
+    directories: Vec<String>,
+}
+
+#[get("/dir/{path:.*}")]
+pub async fn get_directory(path: web::Path<String>) -> Result<HttpResponse, Error> {
+    println!("{:?}", path);
+    let directory = Path::new("content").join(path.to_string());
+    let mut directories = vec![];
+    let mut files = vec![];
+    let mut info = None;
+    let mut entries = async_fs::read_dir(directory).await?;
+    while let Some(entry) = entries.try_next().await? {
+        let a = entry.file_type().await?;
+        if a.is_dir()
+            && let Ok(a) = entry.file_name().into_string() {
+                directories.push(a);
+            }
+        if a.is_file() {
+            if entry.file_name() == "readme.md" {
+                let content_bytes = async_fs::read(entry.path()).await?;
+                let content = String::from_utf8(content_bytes);
+                if let Ok(content) = content {
+                    info = Some(content);
+                }
+                continue;
+            }
+            else if let Ok(a) = entry.file_name().into_string() {
+                files.push(a)
+            }
+        }
+    }
+
+    Ok(HttpResponse::Ok().json(DirectoryListing {
+        info,
+        files,
+        directories,
+    }))
 }
 
 #[cfg(test)]
@@ -21,7 +69,10 @@ mod tests {
     #[test]
     fn api_is_responsive() -> Result<()> {
         let status = reqwest::blocking::get(format!("{URL}/api/v1/hello"))?.status();
-        assert_eq!(status, 200, "Could not reach API. Make sure the backend is running and available in `{URL}` before running tests.");
+        assert_eq!(
+            status, 200,
+            "Could not reach API. Make sure the backend is running and available in `{URL}` before running tests."
+        );
         Ok(())
     }
 }
